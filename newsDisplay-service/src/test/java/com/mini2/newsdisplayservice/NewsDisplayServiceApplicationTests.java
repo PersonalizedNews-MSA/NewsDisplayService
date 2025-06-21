649e5b0552b271105af5638e995ea48db14ed1e3
Sunhokim2 <mcwright@naver.com>
2025-06-21 23:50:18 +0900

    최초 커밋
    기본 서비스 제작 틀입니다.
    여기서부터 마이크로서비스 제작할 것입니다.

---
+++ newsDisplay-service/src/test/java/com/mini2/newsdisplayservice/NewsDisplayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mini2.newsdisplayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsDisplayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
